f45b8b187436c97f731b8764a976ce0c1245a130
Arthur <artur1sharipov@mail.ru>
2025-11-17 05:58:07 +0500

    upd

---
+++ 4.py
@@ -0,0 +1,12 @@
+class NegativValueException(Exception):
+    pass
+
+def check_name(name):
+    if len(name) > 10:
+        raise NegativValueException('Длина более 10 символов')
+    else:
+        print('Успешная регистрация')
+
+if __name__ == '__main__':
+    name = '12345678910'
+    check_name(name)
